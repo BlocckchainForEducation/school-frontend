fced00b1ddaf4cc1befbec654d0753ce1e0e5436
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-07 07:45:08 +0700

    :construction: working on handle  upload file

--- src/views/CreateBureauAccount/index.js
+++ src/views/CreateBureauAccount/index.js
@@ -1,9 +1,13 @@
 import { makeStyles } from "@material-ui/core";
-import React from "react";
+import { useSnackbar } from "notistack";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import DragnDropZone from "../../shared/DragnDropZone";
 import View from "../../shared/View";
+import { getToken } from "../../utils/mng-token";
 import BureauDataExample from "./BureauDataExample";
 import BureauUploadHistory from "./BureauUploadHistory";
+import { addHistoryItem, setPreloadHistory } from "./redux";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,9 +19,40 @@ const useStyles = makeStyles((theme) => ({
 
 export default function CreateBureauAccount() {
   const cls = useStyles();
+  const { enqueueSnackbar } = useSnackbar();
+  const dp = useDispatch();
 
-  async function hdUploadFile() {
-    alert("ok");
+  useEffect(() => {
+    fetchHistory();
+  }, []);
+
+  async function fetchHistory() {
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/bureau-history`, {
+      headers: { Authorization: getToken() },
+    });
+    const result = await response.json();
+    if (!response.ok) {
+      enqueueSnackbar("Fail to load history: " + JSON.stringify(result), { variant: "error", anchorOrigin: { vertical: "top", horizontal: "center" } });
+    } else {
+      dp(setPreloadHistory(result));
+    }
+  }
+
+  async function hdUploadFile(files) {
+    const formData = new FormData();
+    formData.append("excel-file", files[0]);
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/create-bureau`, {
+      method: "POST",
+      headers: { Authorization: getToken() },
+      body: formData,
+    });
+    const result = await response.json();
+    if (!response.ok) {
+      enqueueSnackbar("Some thing went wrong: " + JSON.stringify(result), { variant: "error", anchorOrigin: { vertical: "top", horizontal: "center" } });
+    } else {
+      dp(addHistoryItem(result));
+      enqueueSnackbar("Upload file thành công!", { variant: "success", anchorOrigin: { vertical: "bottom", horizontal: "center" } });
+    }
   }
 
   return (
