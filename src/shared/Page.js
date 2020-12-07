7cb5b66b6814f666c6e889699c91f5d2802fc699
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-07 10:26:26 +0700

    :heavy_minus_sign: remove View cpn, use Page cpn only

--- src/shared/Page.js
+++ src/shared/Page.js
@@ -1,10 +1,20 @@
+import { makeStyles } from "@material-ui/core";
+import PropTypes from "prop-types";
 import React, { forwardRef } from "react";
 import { Helmet } from "react-helmet";
-import PropTypes from "prop-types";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100%",
+    padding: theme.spacing(2.5),
+  },
+}));
 
 const Page = forwardRef(({ children, title = "", ...rest }, ref) => {
+  const cls = useStyles();
+
   return (
-    <div ref={ref} {...rest} style={{ height: "100%" }}>
+    <div ref={ref} {...rest} className={cls.root}>
       <Helmet>
         <title>{title}</title>
       </Helmet>
