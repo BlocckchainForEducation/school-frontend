1d1b33d86fadd1cb03a046f3748cfc0f8151f0dd
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-22 09:40:14 +0700

    :pencil2: show notistack when token invalid, and require re-signin

--- src/layouts/StaffDashboardLayout/index.js
+++ src/layouts/StaffDashboardLayout/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
-import { makeStyles } from "@material-ui/core";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Button, makeStyles } from "@material-ui/core";
 import NavBar from "./NavBar";
 import TopBar from "./TopBar";
 import { getToken } from "src/utils/mng-token";
@@ -8,6 +8,7 @@ import { setProfile } from "src/views/staff/MakeRequest/redux";
 import { useDispatch, useSelector } from "react-redux";
 import Loading from "src/shared/Loading";
 import PerfectScrollbar from "react-perfect-scrollbar";
+import { useSnackbar } from "notistack";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -43,6 +44,8 @@ const StaffDashboardLayout = () => {
   const [isMobileNavOpen, setMobileNavOpen] = useState(false);
   const loading = useSelector((state) => state.profileSlice.fetching);
   const dp = useDispatch();
+  const navigate = useNavigate();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
   useEffect(() => {
     fetchProfile();
@@ -55,7 +58,22 @@ const StaffDashboardLayout = () => {
         headers: { Authorization: getToken() },
       });
       if (!response.ok) {
-        alert(JSON.stringify(await response.json()));
+        enqueueSnackbar("Phiên làm việc đã kết thúc, vui lòng đăng nhập lại!", {
+          anchorOrigin: { vertical: "top", horizontal: "center" },
+          action: (key) => (
+            <Button
+              size="small"
+              style={{ color: "white" }}
+              onClick={(e) => {
+                navigate("/dang-nhap");
+                closeSnackbar(key);
+              }}
+            >
+              Đăng nhập
+            </Button>
+          ),
+        });
+        // alert(JSON.stringify(await response.json()));
       } else {
         dp(setProfile(await response.json()));
       }
