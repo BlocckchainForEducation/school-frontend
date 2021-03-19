a3d2d77d375297e4b081dcf71e8e513d9d1859f8
DinhHoangNam <dinhhoangnam162793@gmail.com>
2021-03-19 14:13:41 +0700

    :shirt: refactor: separate to files

---
+++ src/layouts/TeacherDashboardLayout/TwoFAVerifyStepper.jsx
@@ -0,0 +1,95 @@
+import { Box, Button, Grid, Step, StepLabel, Stepper, TextField, Typography } from "@material-ui/core";
+import axios from "axios";
+import { useSnackbar } from "notistack";
+import React, { useState } from "react";
+import { setToken } from "../../utils/mng-token";
+import { ERR_TOP_CENTER, SUCCESS_TOP_CENTER } from "../../utils/snackbar-utils";
+
+export default function TwoFAStepper({ secret, qrDataURL, setOpenDialog }) {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [OTP, setOTP] = useState([]);
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  function hdChange(e, index) {
+    const clonedOTP = [...OTP];
+    clonedOTP[index] = e.target.value;
+    setOTP(clonedOTP);
+  }
+
+  async function sendOTP() {
+    const otpString = OTP.join("");
+    try {
+      const response = await axios.post("/acc/2fa/verify", { OTP: otpString });
+      if (!response.data.ok) {
+        enqueueSnackbar("Mã OTP không chính xác! Hãy thử lại!", ERR_TOP_CENTER);
+      } else {
+        const newJWToken = response.data.token;
+        setToken(newJWToken);
+        setOpenDialog(false);
+        enqueueSnackbar("Bật cơ chế xác thực 2 bước thành công!", SUCCESS_TOP_CENTER);
+      }
+    } catch (error) {
+      enqueueSnackbar(JSON.stringify(error.response.data), ERR_TOP_CENTER);
+    }
+  }
+
+  return (
+    <Box pb={1}>
+      <Stepper activeStep={activeStep}>
+        <Step key="showSecretToUser">
+          <StepLabel>Nhập mã vào Authenticator</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Xác nhận lại</StepLabel>
+        </Step>
+      </Stepper>
+
+      <Box>
+        {activeStep === 0 && (
+          <Grid container>
+            <Grid item xs={12} sm={4}>
+              <Typography variant="h5">Quét mã QR code</Typography>
+              <img src={qrDataURL} alt="QRCode" />
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <Typography variant="h5">Hoặc nhập mã này</Typography>
+              <Typography>{secret.base32}</Typography>
+            </Grid>
+          </Grid>
+        )}
+        {activeStep === 1 && (
+          <Grid container spacing={2} justify="center">
+            {[...Array(6).keys()].map((index) => (
+              <Grid item>
+                <TextField
+                  inputProps={{ size: 1 }}
+                  variant="outlined"
+                  color="primary"
+                  value={OTP[index]}
+                  onChange={(e) => hdChange(e, index)}
+                ></TextField>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Box>
+
+      <Box mt={3} textAlign="right">
+        <Button disabled={activeStep === 0} onClick={() => setActiveStep(activeStep - 1)} style={{ marginRight: 16 }}>
+          Back
+        </Button>
+        {activeStep === 0 && (
+          <Button variant="contained" color="primary" onClick={() => setActiveStep(activeStep + 1)}>
+            Next
+          </Button>
+        )}
+        {activeStep === 1 && (
+          <Button variant="contained" color="primary" onClick={sendOTP}>
+            Send
+          </Button>
+        )}
+      </Box>
+    </Box>
+  );
+}
--- src/layouts/TeacherDashboardLayout/TwoFactorAuthenDialog.jsx
+++ src/layouts/TeacherDashboardLayout/TwoFactorAuthenDialog.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, FormControlLabel, Grid, Step, StepLabel, Stepper, Switch, TextField, Typography } from "@material-ui/core";
+import { FormControlLabel, Switch, Typography } from "@material-ui/core";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
@@ -8,7 +8,8 @@ import { useSnackbar } from "notistack";
 import QRCode from "qrcode";
 import React, { useState } from "react";
 import { getToken, setToken } from "../../utils/mng-token";
-import { ERR_TOP_CENTER, INFO_TOP_CENTER, SUCCESS_TOP_CENTER } from "../../utils/snackbar-utils";
+import { ERR_TOP_CENTER, INFO_TOP_CENTER } from "../../utils/snackbar-utils";
+import TwoFAStepper from "./TwoFAStepper";
 
 export default function TwoFactorAuthenDialog({ setOpenDialog }) {
   const token = getToken();
@@ -70,97 +71,8 @@ export default function TwoFactorAuthenDialog({ setOpenDialog }) {
           label="Trạng thái"
           labelPlacement="start"
         ></FormControlLabel>
-        {openStepper && <TwoFAVerifyStepper secret={twoFASecret} qrDataURL={qrDataURL} setOpenDialog={setOpenDialog}></TwoFAVerifyStepper>}
+        {openStepper && <TwoFAStepper secret={twoFASecret} qrDataURL={qrDataURL} setOpenDialog={setOpenDialog}></TwoFAStepper>}
       </DialogContent>
     </Dialog>
-  );
-}
-
-function TwoFAVerifyStepper({ secret, qrDataURL, setOpenDialog }) {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [OTP, setOTP] = useState([]);
-
-  const { enqueueSnackbar } = useSnackbar();
-
-  function hdChange(e, index) {
-    const clonedOTP = [...OTP];
-    clonedOTP[index] = e.target.value;
-    setOTP(clonedOTP);
-  }
-
-  async function sendOTP() {
-    const otpString = OTP.join("");
-    try {
-      const response = await axios.post("/acc/2fa/verify", { OTP: otpString });
-      if (!response.data.ok) {
-        enqueueSnackbar("Mã OTP không chính xác! Hãy thử lại!", ERR_TOP_CENTER);
-      } else {
-        const newJWToken = response.data.token;
-        setToken(newJWToken);
-        setOpenDialog(false);
-        enqueueSnackbar("Bật cơ chế xác thực 2 bước thành công!", SUCCESS_TOP_CENTER);
-      }
-    } catch (error) {
-      enqueueSnackbar(JSON.stringify(error.response.data), ERR_TOP_CENTER);
-    }
-  }
-
-  return (
-    <Box pb={1}>
-      <Stepper activeStep={activeStep}>
-        <Step key="showSecretToUser">
-          <StepLabel>Nhập mã vào Authenticator</StepLabel>
-        </Step>
-        <Step>
-          <StepLabel>Xác nhận lại</StepLabel>
-        </Step>
-      </Stepper>
-
-      <Box>
-        {activeStep === 0 && (
-          <Grid container>
-            <Grid item xs={12} sm={4}>
-              <Typography variant="h5">Quét mã QR code</Typography>
-              <img src={qrDataURL} alt="QRCode" />
-            </Grid>
-            <Grid item xs={12} sm={8}>
-              <Typography variant="h5">Hoặc nhập mã này</Typography>
-              <Typography>{secret.base32}</Typography>
-            </Grid>
-          </Grid>
-        )}
-        {activeStep === 1 && (
-          <Grid container spacing={2} justify="center">
-            {[...Array(6).keys()].map((index) => (
-              <Grid item>
-                <TextField
-                  inputProps={{ size: 1 }}
-                  variant="outlined"
-                  color="primary"
-                  value={OTP[index]}
-                  onChange={(e) => hdChange(e, index)}
-                ></TextField>
-              </Grid>
-            ))}
-          </Grid>
-        )}
-      </Box>
-
-      <Box mt={3} textAlign="right">
-        <Button disabled={activeStep === 0} onClick={() => setActiveStep(activeStep - 1)} style={{ marginRight: 16 }}>
-          Back
-        </Button>
-        {activeStep === 0 && (
-          <Button variant="contained" color="primary" onClick={() => setActiveStep(activeStep + 1)}>
-            Next
-          </Button>
-        )}
-        {activeStep === 1 && (
-          <Button variant="contained" color="primary" onClick={sendOTP}>
-            Send
-          </Button>
-        )}
-      </Box>
-    </Box>
   );
 }
